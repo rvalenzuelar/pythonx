461f78ea41ab294af78194761a48da242b982e80
rvalenzuela <raul.valenzuela@colorado.edu>
2014-12-15 18:05:34 -0700

    working on

--- readVAD.py
+++ readVAD.py
@@ -5,16 +5,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# loadtxt open/close the file and retrieves a numpy array
+# open/close the file and retrieves a numpy array
 data=np.loadtxt('swp.1040216150010.NOAA-D.0.0.5_PPI_v2_V3.vad',
                 dtype='float')
 
 azim=data[0]
 veloc=data[1:]
+nanvalue=-32768.
+veloc[veloc==nanvalue]=np.nan
 
-veloc[veloc==-32768.]=np.nan
+index=range(40,41,1)
+for i in index:
+    plt.plot(azim,veloc[i])
 
-plt.plot(azim,veloc[20])
 plt.xlabel('azimuth[deg]')
 plt.ylabel('Radial velocity')
 plt.grid(True)
