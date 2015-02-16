6c5b39d12ede2dc6decf18a33f309a9c8e43402c
rvalenzuela <raul.valenzuela@colorado.edu>
2015-02-16 14:34:11 -0700

    adding comments

--- unfoldVAD_v2.py
+++ unfoldVAD_v2.py
@@ -4,9 +4,8 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import Rbf
-import re
-# from matplotlib import gridspec
+from scipy.interpolate import Rbf   # radial basis function module
+import re                           # regular expression module
 
 
 def run(filepath=''):
