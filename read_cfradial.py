fc81c6638105e977fe2399d96414a74173903327
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-03-30 16:33:04 -0600

    working with single file

--- read_cfradial.py
+++ read_cfradial.py
@@ -22,39 +22,58 @@ stdtape_lons=stdtape_file.variables['LON'][:]
 # close the file
 stdtape_file.close()
 
+# pandas dataframe for standar tape
+d={'lats':stdtape_lats,'lons':stdtape_lons}
+df_stdtape=pd.DataFrame(data=d,index=stdtape_timestamp)
+
 # get a list cfradial files
 nclist = glob.glob('cfrad.*')
+nlist=len(nclist)
 
-# open cfradial file for reading and writing
-cfrad_file = Dataset(nclist[0],'r+') 
+for f in np.arange(nlist):
+	# open cfradial file for reading and writing
+	cfrad_file = Dataset(nclist[f],'r+') 
 
-# get cfradial timestamp
-start_datetime_nparray=cfrad_file.variables['time_coverage_start'][:]
-start_datetime_str="".join(start_datetime_nparray)
-time_format="%Y-%m-%dT%H:%M:%SZ"
-cfrad_start_datetime=pd.to_datetime(start_datetime_str,format=time_format)
-cfrad_time = cfrad_file.variables['time'][:]
-cfrad_secs=pd.to_timedelta(cfrad_time.astype(int),unit='s')
-cfrad_timestamp=cfrad_start_datetime+cfrad_secs
+	# get cfradial timestamp
+	start_datetime_nparray=cfrad_file.variables['time_coverage_start'][:]
+	start_datetime_str="".join(start_datetime_nparray)
+	time_format="%Y-%m-%dT%H:%M:%SZ"
+	cfrad_start_datetime=pd.to_datetime(start_datetime_str,format=time_format)
+	cfrad_time = cfrad_file.variables['time'][:]
+	cfrad_secs=pd.to_timedelta(cfrad_time.astype(int),unit='s')
+	cfrad_timestamp=cfrad_start_datetime+cfrad_secs
 
-# cfradial coordinates
-lats = cfrad_file.variables['latitude'][:]
-lons = cfrad_file.variables['longitude'][:]
+	# remove duplicated timestamps (str)
+	unique_timestamp=cfrad_timestamp.drop_duplicates()
+	nstamps=unique_timestamp.nunique()
+
+	# cfradial coordinates
+	cfrad_lats = cfrad_file.variables['latitude'][:]
+	cfrad_lons = cfrad_file.variables['longitude'][:]
+
+	# pandas dataframe for cfradial file
+	d={'lats':cfrad_lats,'lons':cfrad_lons}
+	df_cfrad=pd.DataFrame(data=d,index=cfrad_timestamp)
+	df_cfrad_new=df_cfrad.copy()
+
+	for t in np.arange(nstamps):
+		timestamp=str(unique_timestamp[t])
+		new_lats=df_stdtape[timestamp]['lats']
+		new_lons=df_stdtape[timestamp]['lons']
+		df_cfrad_new.loc[timestamp,'lats']=new_lats
+		df_cfrad_new.loc[timestamp,'lons']=new_lons
 
 
+	cfrad_file.variables['latitude'][:]=df_cfrad_new['lats'].values
+	cfrad_file.variables['longitude'][:]=df_cfrad_new['lons'].values
 
+	# print ''
+	# print df_cfrad['lats'].values
+	# print ''
+	# print type(df_cfrad_new['lats'].values)
+	# print ''
+	# print df_cfrad_new['2001-01-25 18:44:51']['lats']
+	# print ''
 
-
-
-
-
-# print ''
-# print cfrad_start_datetime
-print ''
-print stdtape_lats
-print ''
-print stdtape_lons
-print ''
-
-# close the file.
-cfrad_file.close()
+	# close the file.
+	cfrad_file.close()
