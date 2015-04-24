0aa1c61b7e073c4c2d94f18605cab41734d089a5
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-04-24 10:56:38 -0600

    added global constants and cleaning

--- make_dtm_profile.py
+++ make_dtm_profile.py
@@ -2,32 +2,33 @@
 # Raul Valenzuela
 # April, 2015
 #
-# See:
-#
+# For process_profile and getElevation see:
 # http://gis.stackexchange.com/questions/59316/
 # python-script-for-getting-elevation-difference-between-two-points
 #
-#
+# For interpolateLine see:
 # http://stackoverflow.com/questions/16015533/get-n-points-on-a-line
+#
+# For destination see:
+# http://www.movable-type.co.uk/scripts/latlong.html
+# http://williams.best.vwh.net/avform.htm#LL
+
 
 def process_profile():
 
-	import math
+
 	import gdal
 	from osgeo import osr,ogr
 	from os.path import expanduser
-	home = expanduser("~")
 
+	home = expanduser("~")
 	demfile = home+'/Github/RadarQC/merged_dem_38-39_123-124_extended.tif'
 	radar_position=(38,1,-123,4)
 	tilt_angle=19.5
-	deg2rad=math.pi/180.0
 
 	layer = gdal.Open(demfile)
 	bands = layer.RasterCount
 	gt =layer.GetGeoTransform()
-
-	# print getElevation(-123.101384, 38.666681,layer,bands,gt)
 
 	startP=[38.44,-123.31]
 	finishP=[38.63,-123.07]
@@ -39,7 +40,6 @@ def process_profile():
 	for point in line:
 		print getElevation(point[1],point[0],layer,bands,gt)
 		# print point[0],point[1]
-
 
 def getElevation(x,y,layer,bands,gt):
 	
@@ -67,5 +67,16 @@ def interpolateLine(start_point,finish_point,number_points):
 		line_points.append((point['lat2'], point['lon2']))
 	return line_points
 
+def destination(start_point,heading,distance):
+	
+	earth_radius=6371 # [km]
+
+
+
+# define global constants
+import math
+pi = math.pi
+deg2rad = pi/180.0 
+rad2deg = 180.0/pi
 
 process_profile()
