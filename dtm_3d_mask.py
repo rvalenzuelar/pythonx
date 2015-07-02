3b2f1485ae41afc97f33d8ce4e0ac9c55fe16f0c
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-07-02 17:10:31 -0600

    whitespace

--- dtm_3d_mask.py
+++ dtm_3d_mask.py
@@ -1,9 +1,14 @@
+# Creates a 3D of masking terrain
 
+# Raul Valenzuela
+# July 2015
 
 import numpy as np
 import gdal
 import os
 import matplotlib.pyplot as plt
+import math
+from os.path import expanduser
 
 
 def print_dimensions(datafile):
@@ -20,10 +25,10 @@ def print_georeference(datafile):
 	print "pixel height [deg]: %s\n" % geot[5]
 
 
-
-dem_file='/home/raul/Github/pythonx/merged_dem_38-39_123-124_extended.tif'
-temp_file='/home/raul/Github/pythonx/temp.tif'
-out_file='/home/raul/Github/pythonx/temp_resamp.tif'
+home = expanduser("~")
+dem_file=home+'/Github/RadarQC/merged_dem_38-39_123-124_extended.tif'
+temp_file=home+'/Github/pythonx/temp.tif'
+out_file=home+'/Github/pythonx/temp_resamp.tif'
 
 
 if os.path.isfile(temp_file):
@@ -67,28 +72,37 @@ data=band.ReadAsArray(0,0,cols,rows)
 datafile=None
 
 plt.figure()
-plt.imshow(data,interpolation='none',cmap='terrain',vmin=0,vmax=800)
+plt.imshow(data,interpolation='none',cmap='terrain',vmin=715,vmax=825)
 plt.colorbar()
 plt.draw()
 
+''' number of verical gates '''
 levels=15
+
+''' creates 3D mask array '''
 dtm_mask=np.zeros((rows,cols,levels))
 
-res=25 # [m] vertical resolution
-for ij in np.ndindex(dtm_mask.shape[:2]):
-	i,j=ij
-	n = int(data[i,j]/res)
-	if n > levels:
-		dtm_mask[i,j] = 1
-	else:
-		dtm_mask[i,j,0:n] = 1
-	
-	# print dtm_mask[i,j]
+''' vertical gate resolution'''
+res=55 # [m] 
 
-# print dtm_mask[:,:,5]
+'''Loop through each pixel of DTM and 
+corresponding vertical column of dtm_mask'''
+for ij in np.ndindex(dtm_mask.shape[:2]):
+
+	'''indices'''
+	i,j=ij
+	
+	'''index of maximum vertical gate to
+	filled with ones (e.g. presence of terrain);
+	works like floor function'''
+	n = int(math.ceil(data[i,j]/float(res)))
+	
+	''' fills verical levels '''
+	dtm_mask[i,j,0:n] = 1
+	
 
 plt.figure()
-plt.imshow(dtm_mask[:,:,2],interpolation='none')
+plt.imshow(dtm_mask[:,:,13],interpolation='none',vmin=0,vmax=1)
 plt.colorbar()
 plt.draw()
 
