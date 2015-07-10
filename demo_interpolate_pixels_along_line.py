a54a366d03698ce3527d12f92b20adaf6fc51938
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-07-10 16:42:13 -0600

    draw pixel line

---
+++ demo_interpolate_pixels_along_line.py
@@ -0,0 +1,66 @@
+# From:
+# http://stackoverflow.com/questions/24702868/python3-pillow-get-all-pixels-on-a-line
+# https://github.com/sachinruk/xiaolinwu/blob/master/xiaolinwu.m
+
+import math
+
+def xiaoline(x0, y0, x1, y1):
+
+	x=[]
+	y=[]
+	dx = x1-x0
+	dy = y1-y0
+	steep = abs(dx) < abs(dy)
+
+	if steep:
+		x0,y0 = y0,x0
+		x1,y1 = y1,x1
+		dy,dx = dx,dy
+
+	if x0 > x1:
+		x0,x1 = x1,x0
+		y0,y1 = y1,y0
+
+	gradient = float(dy) / float(dx)  # slope
+
+	# print ("points(x0,y0,x1,y1): %s,%s,%s,%s" % (x0,y0,x1,y1))
+	# print ("dx,dy: %s,%s" % (dx,dy))
+	# print ("gradient: %s" % gradient)
+
+	""" handle first endpoint """
+	xend = round(x0)
+	yend = y0 + gradient * (xend - x0)
+	xpxl0 = int(xend)
+	ypxl0 = int(yend)
+	x.append(xpxl0)
+	y.append(ypxl0)	
+	x.append(xpxl0)
+	y.append(ypxl0+1)
+	intery = yend + gradient
+
+	""" handles the second point """
+	xend = round (x1);
+	yend = y1 + gradient * (xend - x1);
+	xpxl1 = int(xend)
+	ypxl1 = int (yend)
+	x.append(xpxl1)
+	y.append(ypxl1)	
+	x.append(xpxl1)
+	y.append(ypxl1 + 1)
+
+	""" main loop """
+	for px in range(xpxl0 + 1 , xpxl1):
+		x.append(px)
+		y.append(int(intery))
+		x.append(px)
+		y.append(int(intery) + 1)
+		intery = intery + gradient;
+
+	if steep:
+		y,x = x,y
+
+	coords=zip(x,y)
+
+	return coords
+
+
---
+++ drawing_pixel_lines.py
@@ -0,0 +1,32 @@
+import numpy as np 
+import demo_interpolate_pixels_along_line as interp 
+import matplotlib.pyplot as plt
+
+
+A=np.zeros((101,101))
+
+p0=(10,70)
+p1=(80,1)
+
+coords=interp.xiaoline(p0[0],p0[1],p1[0],p1[1])
+for c in coords:
+	A[c]=1
+
+A[p0]=0.2
+A[p1]=0.8
+
+plt.figure()
+plt.imshow(A.T,interpolation='none',
+				origin='lower',
+				cmap='gist_earth_r',
+				vmin=0,
+				vmax=1)
+plt.grid(which='major')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.text(p0[0],p0[1],'0',fontsize=18,color='r')
+plt.text(p1[0],p1[1],'1',fontsize=18,color='r')
+plt.show()
+
+
+
