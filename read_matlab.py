c0908795151e3d44d381f46ebf8803e1c548d767
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-12-01 13:14:58 -0700

    added base_directory and loop for reading in matfiles

--- read_matlab.py
+++ read_matlab.py
@@ -8,43 +8,74 @@ import scipy.io as sio
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.ticker as mtick
+import os
 
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib.ticker import MultipleLocator
 from scipy.interpolate import interp1d
 from datetime import datetime,timedelta
 
+
+base_directory='/home/rvalenzuela/SPROF/matfiles'
+
+
 def main():
 
-	# mat1=sio.loadmat('/home/raul/Desktop/SPROF/czc_cal_sprof_01023_120gates.mat')
-	# mat2=sio.loadmat('/home/raul/Desktop/SPROF/czc_cal_sprof_01024_120gates.mat')
-	mat1=sio.loadmat('/home/raul/Desktop/SPROF/czc_cal_sprof_01048_120gates.mat')
-	mat2=sio.loadmat('/home/raul/Desktop/SPROF/czc_cal_sprof_01049_120gates.mat')
 
-	ht=mat1['czc_sprof_ht'][0]
-	dayt1=mat1['czc_sprof_dayt']
-	dayt2=mat2['czc_sprof_dayt']
-	
-	dbz1=mat1['czc_sprof_dbz']
-	dbz2=mat2['czc_sprof_dbz']
-	
-	vvel1=mat1['czc_sprof_vvel']
-	vvel2=mat2['czc_sprof_vvel']
+	# case='03'
 
-	dayt1 = np.asarray([datenum_to_datetime(x) for x in dayt1])
-	dayt2 = np.asarray([datenum_to_datetime(x) for x in dayt2])
 
-	dayt=np.hstack((dayt1,dayt2))
 
-	dbz=np.hstack((dbz1.T,dbz2.T))
-	vvel=np.hstack((vvel1.T,vvel2.T))
+	sprof_files =get_filenames('3')
+	mat={}
+	dayt={}
+	dbz={}
+	vvel={}
+	if len(sprof_files)>1:
+
+		for k,s in enumerate(sprof_files):
+			# # mat1=sio.loadmat('/home/raul/Desktop/SPROF/czc_cal_sprof_01023_120gates.mat')
+			# # mat2=sio.loadmat('/home/raul/Desktop/SPROF/czc_cal_sprof_01024_120gates.mat')
+			# mat1=sio.loadmat('/home/raul/Desktop/SPROF/czc_cal_sprof_01048_120gates.mat')
+			# mat2=sio.loadmat('/home/raul/Desktop/SPROF/czc_cal_sprof_01049_120gates.mat')
+
+
+			mat[k]=sio.loadmat(s)
+			ht=mat[k]['czc_sprof_ht'][0]
+			daytin=mat[k]['czc_sprof_dayt']
+			dayt[k]=np.asarray([datenum_to_datetime(x) for x in daytin])
+			dbz[k]=mat[k]['czc_sprof_dbz']
+			vvel[k]=mat[k]['czc_sprof_vvel']
+
+		dayt=np.hstack((dayt[0],dayt[1]))
+		dbz=np.hstack((dbz[0].T,dbz[1].T))
+		vvel=np.hstack((vvel[0].T,vvel[1].T))
+
+	else:
+		mat=sio.loadmat(sprof_files[0])
+
+		ht=mat['czc_sprof_ht'][0]
+		dayt=mat['czc_sprof_dayt']
+		dayt = np.asarray([datenum_to_datetime(x) for x in dayt])
+		dbz=mat['czc_sprof_dbz']
+		vvel=mat['czc_sprof_vvel']
+
+	extent=[0, len(dayt), 0, len(ht)]
+
+	for t in dayt:
+		print t.strftime('%d %H:%M:%S')
+
+	idx_st=find_index(dayt,[2001,1,23,21])
+	idx_end=find_index(dayt,[2001,1,24,1])
+	# idx_st=find_index(dayt,[2001,2,17,17])
+	# idx_end=find_index(dayt,[2001,2,17,19])
 
 	fig,ax=plt.subplots(2,1,sharex=True)
 	im0=ax[0].imshow(dbz,origin='lower',
 						interpolation='nearest',
 						aspect='auto',
 						cmap='nipy_spectral',
-						extent=[0, len(dayt1)+len(dayt2), 0, len(ht)],
+						extent=extent,
 						vmin=-20,
 						vmax=60)
 	add_colorbar(im0,ax[0])
@@ -57,7 +88,7 @@ def main():
 
 	im1=ax[1].imshow(vvel,origin='lower',
 						interpolation='none',
-						extent=[0, len(dayt1)+len(dayt2), 0, len(ht)],
+						extent=extent,
 						aspect='auto',
 						cmap='bwr',
 						vmin=-3,
@@ -69,10 +100,6 @@ def main():
 				weight='bold',
 				transform = ax[1].transAxes)
 
-	# idx_st=find_index(dayt,[2001,1,23,21])
-	# idx_end=find_index(dayt,[2001,1,24,1])
-	idx_st=find_index(dayt,[2001,2,17,17])
-	idx_end=find_index(dayt,[2001,2,17,19])
 	ax[0].set_xlim([idx_st,idx_end])
 
 	fig.subplots_adjust(hspace=.07)	
@@ -81,6 +108,7 @@ def main():
 	ax[1].set_xlabel(r'$\Leftarrow$'+' Time [UTC]')
 	ax[1].set_ylabel('Altitude AGL [km]')
 	plt.draw()
+	# plt.show()
 	plt.show(block=False)
 
 def find_index(datetime_array,t):
@@ -166,5 +194,16 @@ def datenum_to_datetime(datenum):
            + timedelta(minutes=int(minutes)) \
            + timedelta(seconds=round(seconds)) \
            - timedelta(days=366)
+
+def get_filenames(usr_case):
+
+	case='case'+usr_case.zfill(2)
+	casedir=base_directory+'/'+case
+	out=os.listdir(casedir)
+	out.sort()
+	file_sprof=[]
+	for f in out:
+		file_sprof.append(casedir+'/'+f)
+	return file_sprof
 
 main()
