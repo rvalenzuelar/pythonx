f79768277a79fd606cc255f2a371605438d48cfb
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-12-09 11:45:08 -0700

    added plot_spectrum, plot_fields, plot_profile_variance, and plot_profile_valid_gates functions

--- wind_field_2.py
+++ wind_field_2.py
@@ -1,24 +1,31 @@
 
 '''
-check:
-Jessica Lu
-http://www.astrobetter.com/blog/2010/03/03/fourier-transforms-of-images-in-python/
+	Analysis of wind field from P3 synthesis 
+	Raul Valenzuela
+	November, 2015
+
+	For 2D spectrum plotting check Jessica Lu:
+	http://www.astrobetter.com/blog/2010/03/03/fourier-transforms-of-images-in-python/
 '''
 
 
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.gridspec as gridspec
+import seaborn as sns
+
 from scipy import fftpack
 from netCDF4 import Dataset
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
+base_dir='/home/rvalenzuela/P3_v2/synth_prod/'
 
 def main():
 
 	# base_dir='/Users/raulv/Documents/P3/synth/'
-	base_dir='/home/rvalenzuela/P3_v2/synth_prod/'
 
 	case=7
-	leg=4
+	leg=5
 	scase=str(case).zfill(2)
 	sleg=str(leg).zfill(2)
 	synthfile=base_dir+'c'+scase+'/leg'+sleg+'.cdf'
@@ -27,7 +34,168 @@ def main():
 	V = read_synth(synthfile,'F2V')
 	Z = read_synth(synthfile,'z')
 
-	level = 6
+	level = 4
+	# plot_spectrum(U,V,Z,level,scase,sleg)
+	# plot_fields(U,V,Z,level,scase,sleg)
+	
+
+	synth={3:[1,3,5,12,14,16], 7:[3,4,5,6]}
+	plot_profile_variance_wind(synth)
+	plot_profile_variance_dbz(synth)
+	# plot_profile_valid_gates(synth,'wind')
+	# plot_profile_valid_gates(synth,'dbz')
+
+			
+	plt.show(block=False)
+
+
+def plot_profile_variance_wind(synth):
+
+	fig,ax=plt.subplots()
+	colors=get_colors(synth)
+	c=0	
+	for key,value in synth.iteritems():
+		for v in value:
+			scase=str(key).zfill(2)
+			sleg=str(v).zfill(2)
+			synthfile=base_dir+'c'+scase+'/leg'+sleg+'.cdf'
+			U = read_synth(synthfile,'F2U')
+			V = read_synth(synthfile,'F2V')
+			Z = read_synth(synthfile,'z')
+			x=[]
+			y=[]
+			for n,z in enumerate(Z[1:15]):
+				u=U[:,:,n+1]
+				v=V[:,:,n+1]
+				wdir = (np.arctan2(u,v)*180/np.pi)+180.
+				x.append(np.nanvar(wdir))
+				y.append(z)
+			label='Case: '+scase+' Leg: '+sleg
+			ax.plot(x,y,'-',label=label,color=colors[c])
+			ax.set_ylim([0,4])
+			ax.set_xlabel('Wind direction variance [deg^2]')
+			ax.set_ylabel('Altitude MSL [km]')
+			c+=1
+	plt.suptitle('Spatial variance at P3 synth levels ')			
+	plt.draw()
+	plt.legend()
+
+def plot_profile_variance_dbz(synth):
+
+	fig,ax=plt.subplots()
+	colors=get_colors(synth)
+	c=0	
+	for key,value in synth.iteritems():
+		for v in value:
+			scase=str(key).zfill(2)
+			sleg=str(v).zfill(2)
+			synthfile=base_dir+'c'+scase+'/leg'+sleg+'.cdf'
+			DBZ = read_synth(synthfile,'MAXDZ')
+			Z = read_synth(synthfile,'z')
+			x=[]
+			y=[]
+			for n,z in enumerate(Z[1:15]):
+				dbz=DBZ[:,:,n+1]
+				x.append(np.nanvar(dbz))
+				# zz=10**(dbz/10.)
+				# x.append(np.nanvar(zz)) # similar than dbz but in linear scale
+				y.append(z)
+
+			label='Case: '+scase+' Leg: '+sleg
+			ax.plot(x,y,'-',label=label,color=colors[c])
+			ax.set_ylim([0,4])
+			ax.set_xlabel('Reflectivity variance [dBZ^2]')
+			ax.set_ylabel('Altitude MSL [km]')
+			c+=1
+	plt.suptitle('Spatial variance at P3 synth levels ')			
+	plt.draw()
+	plt.legend()
+
+def plot_profile_valid_gates(synth,field):
+
+	fig,ax=plt.subplots()
+
+	if field=='wind':
+		synthvar='F2U' # U and V has same number of valid gates
+	elif field=='dbz':
+		synthvar='MAXDZ'
+
+	colors=get_colors(synth)
+	c=0
+	for key,value in synth.iteritems():
+		for v in value:
+			scase=str(key).zfill(2)
+			sleg=str(v).zfill(2)
+			synthfile=base_dir+'c'+scase+'/leg'+sleg+'.cdf'
+			SVAR = read_synth(synthfile,synthvar)
+			Z = read_synth(synthfile,'z')
+			x=[]
+			y=[]
+			for n,z in enumerate(Z[1:15]):
+				svar=SVAR[:,:,n+1]
+				x.append(svar.count())
+				y.append(z)
+			label='Case: '+scase+' Leg: '+sleg
+			ax.plot(x,y,'-o',label=label,color=colors[c])
+			ax.set_ylim([0,4])
+			ax.set_xlabel('Count')
+			ax.set_ylabel('Altitude MSL [km]')
+			c+=1
+	plt.suptitle('Valid gates in '+field)
+	plt.draw()
+	plt.legend()
+
+def get_colors(synth):
+
+
+	p=['Blues','Reds','Greens']
+	# p=[0,1,2]
+	colors=[]
+	for n,k in enumerate(synth):
+		cs= sns.color_palette(p[n],20,desat=.5)
+		for x,v in enumerate(synth[k]):
+			colors.append(cs[(x*2)+5])
+	return colors
+
+def plot_fields(U,V,Z,level,scase,sleg):
+
+	u=U[:,:,level].T
+	v=V[:,:,level].T
+	wdir = (np.arctan2(u,v)*180/np.pi)+180.
+
+	u_var=np.nanvar(u)
+	v_var=np.nanvar(v)
+	wdir_var=np.nanvar(wdir)
+	var=[u_var, v_var, wdir_var]
+
+	ax=[]
+	im=[]
+	fig = plt.figure(figsize=(15,5))
+	gs = gridspec.GridSpec(1, 3)
+	gs.update(wspace=0.05)
+	ax.append(plt.subplot(gs[0]))
+	ax.append(plt.subplot(gs[1],sharey=ax[0]))
+	ax.append(plt.subplot(gs[2],sharey=ax[0]))
+	im.append(ax[0].imshow(u,interpolation='none',origin='lower',aspect=1,vmin=-7,vmax=7,cmap='RdBu'))
+	im.append(ax[1].imshow(v,interpolation='none',origin='lower',aspect=1,cmap='Blues'))
+	im.append(ax[2].imshow(wdir,interpolation='none',origin='lower',aspect=1,vmin=160,vmax=220,cmap='PRGn'))
+
+	imstr=['U-wind','V-wind','WDIR']
+	for n,x in enumerate(ax):
+		divider = make_axes_locatable(x)
+		cax = divider.append_axes("top", size="2%", pad=0.05)
+		cbar = plt.colorbar(im[n], cax=cax, orientation='horizontal')
+		cbar.ax.xaxis.set_ticks_position('top')
+		cbar.ax.xaxis.set_label_position('top')
+		x.text(10,120,imstr[n],weight='bold')
+		x.text(10,10,'Variance = {:3.2f}'.format(var[n]),weight='bold')
+
+	alt='{:3.2f}'.format(Z[level])
+	plt.suptitle('P3 Synthesis case '+scase+' leg '+sleg+' altitude: '+alt+'km MSL')
+	plt.draw()
+	
+def plot_spectrum(U,V,Z,level,scase,sleg):
+
 	Usp2D = make_2d_spectrum(U[:,:,level])
 	Usp1D = make_1d_spectrum(Usp2D)
 
@@ -37,13 +205,6 @@ def main():
 	SPD=np.sqrt(U**2+V**2)
 	SPDsp2D = make_2d_spectrum(SPD[:,:,level])
 	SPDsp1D = make_1d_spectrum(SPDsp2D)
-
-	print U.shape
-	print Usp2D.shape
-	print Usp1D.shape
-
-
-	
 
 	fig,ax=plt.subplots(1,3,sharey=True)
 	ax[0].semilogy(Usp1D)
@@ -57,10 +218,7 @@ def main():
 	ax[2].set_ylim([1e10,1e16])
 	alt='{:3.2f}'.format(Z[level])
 	plt.suptitle('Power spectrum case '+scase+' leg '+sleg+' altitude: '+alt+'km MSL')
-	
-	plt.show(block=False)
-	
-
+	plt.draw()
 
 def make_2d_spectrum(array):
 
