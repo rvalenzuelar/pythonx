3b40a5960d1cc58cedf6e22e6f9af8fc0ea83036
rvalenzuelar <raul.valenzuela@colorado.edu>
2016-01-07 18:33:56 -0700

    adapted to command line utility

--- make_dir_structur.py
+++ make_dir_structur.py
@@ -1,9 +1,7 @@
+#!/usr/bin/env python
 ''' 
 	Command line utility to make 
 	directory structures
-
-	Usage:
-	$ python make_dir_structure.py c01 leg01 ...legNN
 
 	Raul Valenzuela
 	January 2016
@@ -13,20 +11,31 @@
 import sys
 import os
 
-inarg = sys.argv[1:]
-paths=[]
-for n,s in enumerate(inarg):
-	if n == 0:
-		paths.append(s)
+def main(arg):
+
+	if arg[1] in ['-h', '--help']:
+		usage()
 	else:
-		paths.append('./'+paths[0]+'/'+s+'_cor')
-		paths.append('./'+paths[0]+'/'+s+'_all')
+		inarg = sys.argv[1:]
+		paths=[]
+		for n,s in enumerate(inarg):
+			if n == 0:
+				paths.append(s)
+			else:
+				paths.append('./'+paths[0]+'/'+s+'_cor')
+				paths.append('./'+paths[0]+'/'+s+'_all')
 
-print ' Making directory paths:'
-for p in paths[1:]:
-	print p
-	os.makedirs(p)
-print ' Done\n'
+		print ' Making directory paths:'
+		for p in paths[1:]:
+			print p
+			os.makedirs(p)
+		print ' Done\n'
 
+def usage():
+	s='''
+	Usage:
 
+	$ make_dir_structure.py c01 leg01 ...legNN
+	'''
 
+main(sys.argv)
--- rename.py
+++ rename.py
@@ -1,30 +1,54 @@
+#!/usr/bin/env python
 '''
-	Script for renaming files
+	Command line utility for renaming files
 
 	Raul Valenzuela
 	raul.valenzuela@colorado.edu
 
-	usage:
-	$ python rename.py
 '''
 
 import os
+import sys
 from glob import glob
 
-' current working directory '
-cwd=os.getcwd()
+def main(arg):
 
-' get list with files to process'
-oldl=glob(cwd+'/*.HDF*')
+	if arg[1] in ['-h', '--help']:
+		usage()
+	else:
 
-' make list with new patterns'
-inpattern='/2000'
-oupattern='/'
-newl=[s.replace(inpattern, oupattern) for s in oldl]
+		list_pattern=arg[1]
+		search_pattern=arg[2]
+		replace_pattern=arg[3]
 
-' rename with new pattern '
-for a,b in zip(oldl,newl):
-	print a
-	print b
-	os.rename(a,b)
+		' current working directory '
+		cwd=os.getcwd()
 
+		' get list with files to process'
+		# oldl=glob(cwd+'/*.HDF*')
+		oldl=glob(cwd+'/'+list_pattern)
+
+		' make list with new patterns'
+		# inpattern='/2000'
+		# oupattern='/'
+		# newl=[s.replace(inpattern, oupattern) for s in oldl]
+		newl=[s.replace(search_pattern, replace_pattern) for s in oldl]
+
+		' rename with new pattern '
+		for a,b in zip(oldl,newl):
+			print a
+			print b
+			os.rename(a,b)
+
+def usage():
+	s='''
+	Usage:
+
+	$ rename.py [list_pattern] [search_pattern] [replace_pattern]
+
+	ex:
+	$ rename.py *.HDF* /2000 /
+	'''
+	print s
+
+main(sys.argv)
