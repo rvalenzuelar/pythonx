7abb2495c0d9dcd1582b329436f5479166d5fa7b
rvalenzuelar <raul.valenzuela@colorado.edu>
2016-02-04 14:11:04 -0700

    new file

---
+++ Circlem.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+    Make circle pojected on a map
+
+    Raul Valenzuela
+    January, 2106
+"""
+
+import numpy as np
+from geographiclib.geodesic import Geodesic
+
+
+
+def pick_circle_point(lon, lat, az, radius):
+  
+    gd = Geodesic.WGS84.Direct(lat, lon, az,  radius)  
+    lon2=gd['lon2']
+    lat2=gd['lat2']
+
+    return lon2,lat2
+
+def circle(m, olat, olon, radius):
+    """
+    m = basemap instance
+    olon = origin lon
+    olat = origin lat
+    radius = radius
+    """
+    X = []
+    Y = []
+    for azimuth in range(0, 360):
+        lon2, lat2 = pick_circle_point(olon, olat, azimuth, radius)
+        X.append(lon2)
+        Y.append(lat2)
+    X.append(X[0])
+    Y.append(Y[0])
+
+    proj_x, proj_y = m(X,Y)
+    return zip(proj_x, proj_y)
