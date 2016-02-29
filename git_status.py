facdd16209dc744bb31a133335648fe99844f053
rvalenzuelar <raul.valenzuela@colorado.edu>
2016-02-29 12:42:37 -0700

    new file

---
+++ git_status.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+'''
+    Command line utility for listing changes in Git repos.
+
+    This file has to be located in a directory included
+    in the environmental variable PATH
+
+    Raul Valenzuela
+    January, 2016
+    raul.valenzuela@colorado.edu
+'''
+
+import os
+import git
+from ctext import ctext
+
+GITPATH = '/home/rvalenzuela/Github'
+DIRS = os.listdir(GITPATH)
+DIRS.sort()
+
+for D in DIRS:
+    currentdir = ctext(GITPATH + '/' + D)
+    g = git.cmd.Git(currentdir.text)
+    status = g.status()
+    if 'nothing to commit' in status:
+        print currentdir.green()
+    else:
+        print currentdir.red()
+    print status
+print ''
