bbeb0a217990bf8088c04abf7f7b5f7c4ea99eb7
raul <raul.valenzuela@colorado.edu>
2016-06-01 16:31:40 -0600

    better string parsing

--- download_cfsr.py
+++ download_cfsr.py
@@ -22,21 +22,29 @@ dates = pd.date_range(start=start,end=end,freq='6H')
 
 root = 'http://nomads.ncdc.noaa.gov/modeldata/cmd_pgbh'
 
-url_string='{0}/{1}/{2}/{3}/{4}'
+url_in='{0}/{1}/{2}/{3}/{4}'
+url_out='{0}/{1}/{2}'
 
-out_dir='/home/raul/CFSR/'
+out_dir='/home/raul/CFSR'
+
+print_str='Downloading ({0}/{1}) {2}'
 
 total = dates.size
 
-print_str='Downloading ({0}/{1}) {2}'
 
 for n,date in enumerate(dates):
 	y = str(date.year)
 	m = str(date.month).zfill(2)
 	d = str(date.day).zfill(2)
 	H = str(date.hour).zfill(2)
+
+	if not os.path.isdir(out_dir+'/'+y):
+		os.makedirs(out_dir+'/'+y)
+
 	file_name = 'pgbhnl.gdas.'+y+m+d+H+'.grb2'
-	url = url_string.format(root,y,y+m,y+m+d,file_name)
-	print(print_str.format(n+1,total,file_name))
-	urllib.urlretrieve(url,out_dir+file_name)
+	urli = url_in.format(root,y,y+m,y+m+d,file_name)
+	urlo = url_out.format(out_dir,y,file_name)
+
+	print(print_str.format(n+1,total,urlo))
+	urllib.urlretrieve(urli,urlo)
 
