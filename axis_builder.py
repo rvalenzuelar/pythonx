e07b95b943d984188ca648c3bd22be0dad3c89e4
Raul Valenzuela <raul.valenzuela@colorado.edu>
2016-10-20 21:29:41 -0600

    last update

--- axis_builder.py
+++ axis_builder.py
@@ -4,16 +4,21 @@
     raul.valenzuela@colorado.edu
 
 """
-
-import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
-import numpy as np
 
 plt.ioff()
 
+import matplotlib.gridspec as gridspec
+import numpy as np
+import string
+
+
 
 def specs(rows=None, cols=None, row_ratio=None, col_ratio=None,
-          figsize=None, grid=False):
+          figsize=None, return_grid=False, name_loc='upper-left',
+          hide_labels_in=None, hide_xlabels_in=None,
+          hide_ylabels_in=None,
+          hspace=None, wspace=None):
 
     if figsize is None:
         fig = plt.figure()
@@ -28,13 +33,46 @@ def specs(rows=None, cols=None, row_ratio=None, col_ratio=None,
     axes = list()
     for ax in gs:
         axes.append(plt.subplot(ax))
+    axes = np.array(axes)
 
-        fig.axes.append(plt.subplot(ax))
-
-    if grid is True:
-        axes = np.array(axes).reshape((2, 3))
+    if isinstance(name_loc,str):
+        if name_loc == 'upper-left':
+            loc_coord = (0.05, 0.90)
+        elif name_loc == 'upper-right':
+            loc_coord = (0.90, 0.90)
     else:
-        axes = np.array(axes)
+        loc_coord = name_loc
+
+    pname = iter(list(string.ascii_lowercase)[:rows*cols])
+    for p, ax, in zip(pname, axes):
+        transf = ax.transAxes
+        ax.text(loc_coord[0], loc_coord[1], '({})'.format(p),
+                fontsize=15,
+                weight='bold',
+                transform=transf,
+                color=(0.3, 0.3, 0.3))
+
+    if hide_labels_in is not None:
+        for h in hide_labels_in:
+            axes[h].set_xticklabels('')
+            axes[h].set_yticklabels('')
+
+    if hide_xlabels_in is not None:
+        for h in hide_xlabels_in:
+            axes[h].set_xticklabels('')
+
+    if hide_ylabels_in is not None:
+        for h in hide_ylabels_in:
+            axes[h].set_yticklabels('')
+
+    if return_grid is True:
+        axes = axes.reshape((rows, cols))
+
+    if hspace is not None:
+        plt.subplots_adjust(hspace=hspace)
+
+    if wspace is not None:
+        plt.subplots_adjust(wspace=wspace)
 
     return fig, axes
 
