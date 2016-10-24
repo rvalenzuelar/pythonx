d1fe88a0650dd057651e61a3a414601c850d75e7
Raul Valenzuela <raul.valenzuela@colorado.edu>
2016-10-24 10:20:20 -0600

    last update

--- axis_builder.py
+++ axis_builder.py
@@ -6,7 +6,7 @@
 """
 import matplotlib.pyplot as plt
 
-plt.ioff()
+# plt.ioff()
 
 import matplotlib.gridspec as gridspec
 import numpy as np
@@ -14,11 +14,13 @@ import string
 
 
 
-def specs(rows=None, cols=None, row_ratio=None, col_ratio=None,
+def specs(rows=None, cols=None, id_panels=True,
+          row_ratio=None, col_ratio=None,
           figsize=None, return_grid=False, name_loc='upper-left',
           hide_labels_in=None, hide_xlabels_in=None,
-          hide_ylabels_in=None,
-          hspace=None, wspace=None):
+          hide_ylabels_in=None, show_grid=False,
+          hspace=None, wspace=None,left=None,right=None,
+          top=None, bottom=None):
 
     if figsize is None:
         fig = plt.figure()
@@ -35,22 +37,27 @@ def specs(rows=None, cols=None, row_ratio=None, col_ratio=None,
         axes.append(plt.subplot(ax))
     axes = np.array(axes)
 
-    if isinstance(name_loc,str):
-        if name_loc == 'upper-left':
-            loc_coord = (0.05, 0.90)
-        elif name_loc == 'upper-right':
-            loc_coord = (0.90, 0.90)
-    else:
-        loc_coord = name_loc
+    if id_panels:
+        if isinstance(name_loc,str):
+            if name_loc == 'upper-left':
+                loc_coord = (0.05, 0.90)
+            elif name_loc == 'upper-right':
+                loc_coord = (0.90, 0.90)
+        else:
+            loc_coord = name_loc
 
-    pname = iter(list(string.ascii_lowercase)[:rows*cols])
-    for p, ax, in zip(pname, axes):
-        transf = ax.transAxes
-        ax.text(loc_coord[0], loc_coord[1], '({})'.format(p),
-                fontsize=15,
-                weight='bold',
-                transform=transf,
-                color=(0.3, 0.3, 0.3))
+        pname = iter(list(string.ascii_lowercase)[:rows*cols])
+        for p, ax, in zip(pname, axes):
+            transf = ax.transAxes
+            ax.text(loc_coord[0], loc_coord[1], '({})'.format(p),
+                    fontsize=15,
+                    weight='bold',
+                    transform=transf,
+                    color=(0.3, 0.3, 0.3))
+
+    if show_grid:
+        for ax in axes:
+            ax.grid(True)
 
     if hide_labels_in is not None:
         for h in hide_labels_in:
@@ -73,6 +80,18 @@ def specs(rows=None, cols=None, row_ratio=None, col_ratio=None,
 
     if wspace is not None:
         plt.subplots_adjust(wspace=wspace)
+
+    if left is not None:
+        plt.subplots_adjust(left=left)
+
+    if right is not None:
+        plt.subplots_adjust(right=right)
+
+    if top is not None:
+        plt.subplots_adjust(top=top)
+
+    if bottom is not None:
+        plt.subplots_adjust(bottom=bottom)
 
     return fig, axes
 
